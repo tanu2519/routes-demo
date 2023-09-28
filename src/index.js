b8853b2920b95820ed33e8402451e0453c2c0fe1
Tanya Sharma <tanu282501@gmail.com>
2023-09-28 13:29:13 +0530

    only display ui  fix

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider basename="/routes-demo" router={router} />
   </React.StrictMode>
 );
 reportWebVitals();
